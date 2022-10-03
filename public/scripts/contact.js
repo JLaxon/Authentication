4878df2cb94f0ecf7cbb3d5fefee6e2448b0bcf0
JLaxon <jlaxon@my.centennialcollege.ca>
2022-10-02 22:44:51 -0400

    Adding content to pages and the styling

---
+++ public/scripts/contact.js
@@ -0,0 +1,8 @@
+// function to get info from contact page
+function getInfo() {
+    var fName = document.getElementById("fName");
+    var fName = document.getElementById("lName");
+    var fName = document.getElementById("phone");
+    var fName = document.getElementById("email");
+    var fName = document.getElementById("message");
+}
